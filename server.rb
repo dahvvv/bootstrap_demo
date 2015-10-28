d839d68e7eb87e08104f5dfe4b325c935a6c915a
Dave Koenig <davidjosephkoenig@gmail.com>
2015-10-28 02:23:19 -0400

    post to upload saves image

--- server.rb
+++ server.rb
@@ -4,3 +4,12 @@ Bundler.require
 get '/' do
 	erb :index
 end
+
+post '/upload' do
+	@filename = params[:file][:filename]
+	file = params[:file][:tempfile]
+
+	File.open("./public/images/#{@filename}", "wb") do |f|
+		f.write(file.read)
+	end
+end
