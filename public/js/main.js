ddd6e7cba24aa83fd26058c15c3bd3d97fb4b67c
Dave Koenig <davidjosephkoenig@gmail.com>
2015-10-28 01:39:17 -0400

    add function to check for html5 storage

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
 console.log(":0");
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
