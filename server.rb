086066603d48cc56c27d4d1941d710fa90844796
Dave Koenig <davidjosephkoenig@gmail.com>
2015-10-29 19:12:37 -0400

    remove post /upload

--- server.rb
+++ server.rb
@@ -4,13 +4,3 @@ Bundler.require
 get '/' do
 	erb :index
 end
-
-post '/upload' do
-	file = params[:file][:tempfile]
-
-	File.open("./public/images/pet", "wb") do |f|
-		f.write(file.read)
-	end
-
-	redirect to('/')
-end
